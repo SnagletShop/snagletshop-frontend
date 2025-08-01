dd77ad40225a1aa57827d3900d243923aaa82cd0
SnagletShop <snagletshop@gmail.com>
2025-08-01 11:37:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2530,7 +2530,19 @@ function loadProducts(category, sortBy = "NameFirst", sortOrder = "asc") {
 
         const addToCartBtn = document.createElement("button");
         addToCartBtn.className = "add-to-cart";
-        addToCartBtn.textContent = TEXTS.PRODUCT_SECTION.ADD_TO_CART;
+
+        // Use flexbox to align text and SVG
+        addToCartBtn.innerHTML = `
+        <span style="display: flex; align-items: center;">
+          ${TEXTS.PRODUCT_SECTION.ADD_TO_CART}
+          <svg class="cart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+              stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>
+      `;
+
+
         addToCartBtn.addEventListener("click", () => {
             addToCart(
                 product.name,
@@ -2542,6 +2554,8 @@ function loadProducts(category, sortBy = "NameFirst", sortOrder = "asc") {
                 (product.productOptions && product.productOptions[1]) || ""
             );
         });
+
+
 
         quantityControls.append(decBtn, quantitySpan, incBtn);
         quantityContainer.append(quantityControls, addToCartBtn);
